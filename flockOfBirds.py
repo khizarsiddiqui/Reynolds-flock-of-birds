0a0077386ae0e40fad38184829332b7eb98e210c
khizarsiddiqui <khizarsiddiqui10@gmail.com>
2022-09-08 04:14:02 +0500

    first commit

---
+++ flockOfBirds.py
@@ -0,0 +1,5 @@
+# Initializing the project
+# Main contributors of this repository are safimuhammad and khizarsiddiqui
+# flock of birds simulation will be carried out by using the boids simulation model.
+
+# first commit from khizarsiddiqui
